989d2444d895362afd07ba116b7377ff116efd4f
Nico Wagner <info@ni-wa.de>
2023-08-21 23:34:39 +0200

    feat: added new ExpressServer handle class as entrypoint (for better testing and process exit handling)

--- src/index.ts
+++ src/index.ts
@@ -5,40 +5,84 @@ new Config(dbConfig('mongodb', 'rest-api'));
 import { router } from './routes';
 
 logger.level = 'silly';
-const app = new App(router);
 
-if (process.env.NODE_ENV !== 'test') {
-  logger.info('Trying to connect to database...');
-  //region DB Connection
-  /** MONGOOSE:
-   * mongoose
-   *     .connect(getMongoConnectionString(), { authSource: 'admin' })
-   *     .then((res) => {
-   *       //AppDataSource.initialize().then((_r) => {
-   *       app.afterMiddlewares(new MaintenanceMiddleware(false).ExpressMiddleWare);
-   *       app.init();
-   *       app.getServer().set('etag', false);
-   *       app.listen();
-   *       dataSource = res;
-   *     });
-   */
-  /** MYSQL (typeorm):
-   *  Mysql.createAppDataSource({
-        synchronize: true, // or false in production
-        entities: [], //array of typeorm Entities
-      });
-      Mysql.getAppDataSource()
-        .initialize()
-        .then((res) => {
-          app.init();
-          app.getServer().set('etag', false);
-          app.listen();
-        });
+export class ExpressServer {
+  private app: App;
 
-      @example Mysql.getAppDataSource() returns the datasource for typeorm => Mysql.getAppDataSource().getRepository(<entity>)
+  constructor(start = true) {
+    if (start) this.init();
+  }
+
+  /**
+   * Main Server Startup Function
+   * @param cb - Callback function if database is connected
    */
-  //endregion
-} else {
-  app.init();
-  app.listen();
+  init(cb?: (err?: unknown) => void) {
+    this.app = new App(router);
+    this.app.initializeHttpServer();
+    // this.app.setCorsOptions({
+    //   origin: Config.getConfig<ICorsConfig>().get('cors.origin'),
+    // });
+    //logger.info('Trying to connect to database...');
+    /** without any database or so */
+    this.app.init();
+    this.app.getServer().set('etag', false);
+    this.app.listen(Config.getConfig().get('port'));
+    cb?.();
+    //region DB Connection
+    /** MONGOOSE:
+     * mongoose
+     *     .connect(getMongoConnectionString(), { authSource: 'admin' })
+     *     .then((res) => {
+     *       //AppDataSource.initialize().then((_r) => {
+     *       app.afterMiddlewares(new MaintenanceMiddleware(false).ExpressMiddleWare);
+     *       app.init();
+     *       app.getServer().set('etag', false);
+     *       app.listen();
+     *       dataSource = res;
+     *     });
+     */
+    /** MYSQL (typeorm):
+         Mysql.createAppDataSource({
+         synchronize: true, // or false in production
+         logging: false,
+         entities: [], //array of typeorm Entities
+         });
+         Mysql.getAppDataSource()
+         .initialize()
+         .then(() => {
+         this.app.init();
+         this.app.getServer().set('etag', false);
+         this.app.listen(Config.getConfig().get('port'));
+         cb?.();
+         })
+         .catch((e) => {
+         logger.error('Error while connection to DB');
+         logger.error(e);
+         cb?.(e);
+         });
+         //endregion
+
+         @example Mysql.getAppDataSource() returns the datasource for typeorm => Mysql.getAppDataSource().getRepository(<entity>)
+         */
+    //endregion
+  }
+
+  end = (signal: string) => {
+    logger.info(`Processing Signal: ${signal}`);
+    // try {
+    //   //await Mysql.getAppDataSource().destroy();
+    // } catch (e) {
+    //   /* no error handling */
+    // }
+    this.app.close();
+  };
+}
+
+//start server if not in test environment
+if (Config.getConfig().get('env') !== 'test') {
+  const server = new ExpressServer();
+
+  process.on('SIGINT', server.end);
+  process.on('SIGTERM', server.end);
 }
